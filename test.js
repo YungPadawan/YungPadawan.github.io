6433a6074c7231e2679a647258444a7c6635ac1d
YungPadawan <jack.phillips@me.com>
2014-07-26 22:09:25 -0700

    the site

---
+++ test.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+    $('.spinner').hide();
+    $('p').hide();
+    
+ $('.spinner').fadeIn('slow');
+ 
+ $('.Three-Dee').delay(3000).fadeIn('slow');
+ $('.Three-Dee-Loading').delay(3500).fadeIn('slow');
+});
